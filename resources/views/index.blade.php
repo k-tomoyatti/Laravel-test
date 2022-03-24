0fe813d9505b012b933a5767aa49201f9b4b088e
k-tomoyatti <kumatomo.0610@outlook.jp>
2022-03-24 22:54:22 +0900

    タスク登録機能実装中

---
+++ resources/views/index.blade.php
@@ -0,0 +1,36 @@
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <title>to-doリスト</title>
+</head>
+
+<body>
+    <header class="TODO-header">
+        <h1 class="TODO-header-title">TODOリスト</h1>
+    </header>
+
+    <div>
+        <input type="text">
+        <button>追加</button>
+    </div>
+
+
+
+    <!-- <table>
+    <thead>
+        <tr>
+            <th colspan="2">The table header</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>The table body</td>
+            <td>with two columns</td>
+        </tr>
+    </tbody> -->
+</table>
+</body>
+</html>
