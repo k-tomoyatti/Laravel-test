71a96ad8f3488d6af754ba316845f5bfbdd69863
k-masasa <kuangshixionggu@gmail.com>
2022-03-30 13:18:38 +0900

    #1 レイアウト調整

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,71 +6,38 @@
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <title>to-doリスト</title>
 </head>
-
 <body>
     <header class="header">
         <h1 class="title">TODOリスト</h1>
     </header>
-
     <div class="add-task">
         <form action="{{ url('/task')}}" method="post">
             {{ csrf_field() }}
             <input type="text" class="input" placeholder="タスク名" name="task">
             <!-- <a href="" class="btn btn--red btn--radius btn--cubic">追加<i class="fas fa-angle-right fa-position-right"></i></a> -->
-            <input type="submit" name="add">
+            <input class="add-btn" type="submit" name="add">
         </form>
     </div>
-
-    <div class="task-table">
-        <!-- <table  class="task-table" border="1">
-        <thead>
-            <tr>
-                <th colspan="2">タスク名</th>
-                <th colspan="2">     </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>        
-                @foreach ($tasks as $task)
-                <div>{{$task->name}}</div>
-                @endforeach
-                </td>
-                <td>削除</td>
-            </tr>
-        </tbody>
-        </table> -->
-        
-
-        @foreach ($tasks as $task)
-                <div>{{$task->name}}</div>
-
-        <form method= "post">
-            <tr>
-                <input type= "hidden" name= "id" value= "<?php echo $task["id"]; ?>">
-                <td>
-                    <input type="text" name="text" value=<?php echo $task["text"]; ?> task>
-                </td>
-                <td>
-                    <?php echo $task["created_at"]; ?>
-                </td>
-                <td>
-                    <button type="submit" name="update">更新する</button>
-                </td>
-                <td>
-                    <button type="submit" name="delete">削除する</button>
-                </td>
-            </tr>
+    <div class="tasks-table">
+        <table>
+            @foreach ($tasks as $task)
+                <tr>
+                    <!-- TODO: 別Issueで更新・削除の実装時、formタグを復活させる -->
+                    <!-- <form method="post"> -->
+                        <input type= "hidden" name= "id" value="{{ $task['id'] }}">
+                        <td>
+                            <input class="task-name" type="text" name="task-name" value="{{ $task['name'] }}">
+                        </td>
+                        <td>
+                            <button>更新</button>
+                        </td>
+                        <td>
+                            <button>削除</button>
+                        </td>
+                    <!-- </form> -->
+                </tr>
             @endforeach
-        </form>
-    </table>
-    
-
-
+        </table>
     </div>
-
-
-
-
 </body>
 </html>
